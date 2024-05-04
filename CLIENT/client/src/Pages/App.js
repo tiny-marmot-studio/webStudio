50d36a504cd2018950cdd4f52a541e0a4d3cce63
weiz0123 <76544381+weiz0123@users.noreply.github.com>
2024-05-04 19:15:09 -0400

    intro page

--- CLIENT/client/src/Pages/App.js
+++ CLIENT/client/src/Pages/App.js
@@ -1,14 +1,16 @@
 import { Fragment } from "react";
 import "./App.css";
-
+import IMAGES from './Images/index.js';
 function App() {
     return (
-        <Fragment>
-            <div className="bg"/>
-            <div className="hexagon"/>
-
+        <Fragment >
+            <div className="background">
+                <img src={IMAGES.logo_frame} alt="Logo" className="logo_frame" />
+                <button  class="explore-button">E X P L O R E </button>
+            </div>
         </Fragment>
     );
 }
 
 export default App;
+ 
